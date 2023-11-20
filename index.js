3af896f3dc826c293ed514b90a0d8660212a12ce
Kaitlyn Nowicki <katie.nowicki024@gmail.com>
2023-11-20 09:39:01 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (collection, name) {
+    const newCollection = [];
+
+for (const user of collection) {
+    if (user.toLowerCase() === name.toLowerCase()) {
+        newCollection.push(user);
+    
+    }
+}
+return newCollection;
+}
+
+function fuzzyMatch (collection, letters) {
+    const newCollection = [];
+
+    for (const user of collection) {
+        if (user.startsWith(letters)) {
+        newCollection.push(user);
+    }
+}
+return newCollection;
+}
+
+function matchName (collection, name) {
+    const newCollection = [];
+
+    for (const user of collection) {
+        if (user.name === name) {
+            newCollection.push(user);
+        }
+    }
+return newCollection;
+}
